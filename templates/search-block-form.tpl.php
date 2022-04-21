de2e1caa3e1cc305c940b07d49ef5e94b7f94c6c
Eric Rasmussen <ericrasmussen1@gmail.com>
2022-04-21 10:29:27 -0500

    Update search-block-form.tpl.php

--- templates/search-block-form.tpl.php
+++ templates/search-block-form.tpl.php
@@ -1,4 +1,4 @@
+<label class="element-invisible" for="q">Search </label>
 <div class="dcf-input-group">
-  <label class="element-invisible" for="q">Search </label>
   <?php print $search_form; ?>
 </div>
